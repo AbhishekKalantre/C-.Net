981016ec8332115ad898d8306405683421591e82
AbhishekKalantre <abhishekkalantre123@gmail.com>
2025-02-10 20:48:12 +0530

    Add files via upload

---
+++ Assignment-2/MCA_Student_App/frm_Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCA_Student_App
+{
+    public partial class Frm_Login : Form
+    {
+        public Frm_Login()
+        {
+            InitializeComponent();
+        }
+        //SqlConnection Con = new SqlConnection(@"Data Source=.;Initial Catalog=Student_Login_DB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            if(tb_Username.Text == "A" && tb_Password.Text == "A")
+            {
+                MessageBox.Show("Login Successful","Welcome");
+
+                frm_add_student_detail sd = new frm_add_student_detail();
+                sd.Show();
+                this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Login Failed Enter Correct Username/Password.", "failure");
+            }
+            tb_Password.Clear();
+            tb_Username.Clear();
+
+            tb_Username.Focus();
+
+        }
+    }
+}
